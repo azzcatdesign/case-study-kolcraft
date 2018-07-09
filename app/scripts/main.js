b483146568e25e0c7efb3cf03021a898b680e6f0
Catherine Azzarello <azzcatdesign@gmail.com>
2018-07-09 11:25:36 -0700

    More than I need to version control, but better safe than sorry! INITIAL COMMIT

---
+++ app/scripts/main.js
@@ -0,0 +1,3 @@
+// Scroll up button from https://simplescrollup.programadorwebvalencia.com/
+
+// @codekit-prepend '../../node_modules/simplescrollup/simplescrollup.js'
---
+++ build/app/scripts/sw/runtime-caching.js
@@ -0,0 +1,1 @@
+!function(o){"use strict";o.toolbox.router.get("/(.*)",o.toolbox.fastest,{origin:/\.(?:googleapis|gstatic)\.com$/})}(self);
